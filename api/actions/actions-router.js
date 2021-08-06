0470c493c69cc31355fdcac3af64a3c7207d5eba
sirakisaias <sirakisaias@gmail.com>
2021-08-06 16:54:25 -0700

    final MVP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,4 +4,59 @@ const Actions = require('./actions-model');
 
 const router = express.Router();
 
+const { validateActionsId, validateActions } = require('./actions-middlware')
+
+
+//GET api/actions
+router.get('/', (req, res, next) => {
+    Actions.get()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
+})
+
+//GET api/actions by id
+router.get('/:id', validateActionsId, (req, res ) => {
+    res.json(req.Actions)
+})
+
+//POST api/projects
+router.post('/', validateActions, (req, res, next) => {
+    Actions.insert(req.body)
+    .then(post => {
+        res.json(post)
+    })
+    .catch(next)
+})
+
+//Put api/actions 
+router.put('/:id', async (req, res, next) => {
+    
+    const {id} = req.params;
+    const body = req.body;
+
+    if (!body.description || !body.notes) {
+        res.status(400).json({message: 'fields required'})
+    } else {
+        
+        try {
+            const project = await Actions.update(id, body);
+            res.status(200).json(project)
+        } catch  {
+            next()
+        }
+    }
+})
+
+//DELETE
+router.delete('/:id', validateActionsId, (req, res, next) => {
+    Actions.remove(req.project.id)
+    .then(() => {
+        res.status(200).json({message: 'deleted'})
+    })
+    .catch(next)
+})
+
+
 module.exports = router

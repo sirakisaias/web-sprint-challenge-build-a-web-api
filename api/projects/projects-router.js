45b8fb8654cb122c98d071d071e07b22fb3e170c
sirakisaias <sirakisaias@gmail.com>
2021-08-06 15:04:04 -0700

    finished get by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,18 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
+//GET api/projects by id
+router.get('/:id', (req, res, next) => {
+    const {id} = req.params
+    Projects.get(id)
+    .then(project =>{
+        if (project){
+            res.json(project)
+        } else {
+            res.status(404).json({ message: 'project not found'})
+        }
+    })
+    .catch(next)
+})
+
 module.exports = router
